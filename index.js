c449f685f444b22f57c2c7a841a6e0cf9d5c7490
jeevjacobgeorge <jeev@J4Hp>
2023-12-09 18:04:16 +0530

    Added css file contents

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("superhero")
+favouriteMovieGenre("space")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
-favouriteFruit("watermelon")
+favouriteFruit("avocado")
 
 // light, dark
 favouriteMode("dark")
 
 // sharp, soft, round
-favouriteEdgeStyle("round")
+favouriteEdgeStyle("soft")
 
 
 
